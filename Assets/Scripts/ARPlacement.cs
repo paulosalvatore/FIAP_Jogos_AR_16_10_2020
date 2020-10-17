17523b1e3a03df0698cdd3c15bb9c94071be048e
Paulo Salvatore <paulo@salvatore.tech>
2020-10-16 21:24:48 -0300

    Target Marker posicionando baseado no ARRaycast

--- Assets/Scripts/ARPlacement.cs
+++ Assets/Scripts/ARPlacement.cs
@@ -1,14 +1,41 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class ARPlacement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private void Start()
-    {
-    }
+    [Header("Components")]
+    [SerializeField]
+    private ARRaycastManager arRaycastManager;
 
-    // Update is called once per frame
+    [SerializeField]
+    private GameObject spider;
+
+    [SerializeField]
+    private GameObject targetMarker;
+
     private void Update()
     {
+        var x = Screen.width / 2;
+        var y = Screen.height / 2;
+
+        var screenCenter = new Vector2(x, y);
+
+        var hitResults = new List<ARRaycastHit>();
+
+        arRaycastManager.Raycast(screenCenter, hitResults, TrackableType.Planes);
+
+        if (hitResults.Count > 0)
+        {
+            var hit = hitResults[0];
+
+            transform.SetPositionAndRotation(hit.pose.position, hit.pose.rotation);
+
+            if (!targetMarker.activeSelf)
+            {
+                targetMarker.SetActive(true);
+            }
+        }
     }
 }
